f48019d13c375e90106ac977c13baea7b610c89d
Geraldinne Paola Jaramillo Avendano <geraldinne.jaramillo@globant.com>
2026-02-09 10:49:12 -0500

    feat se crean los entpoints

--- src/main/java/dataPawBE/demo/controllers/PetVaccinationController.java
+++ src/main/java/dataPawBE/demo/controllers/PetVaccinationController.java
@@ -5,7 +5,11 @@ import dataPawBE.demo.models.PetVaccination;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +20,29 @@ public class PetVaccinationController {
     @Autowired
     PetVaccinationService petVaccinationService;
 
+    //Entpoint para activar Guardar una vacunación
     @PostMapping
     public ResponseEntity <PetVaccination> save(@RequestBody PetVaccination data){
       PetVaccination saveResponseAPI = this.petVaccinationService.savePetVaccination(data);
       return ResponseEntity.status(HttpStatus.OK).body(saveResponseAPI);
+    };
+
+    //Entpoint para activar obtener todas las vacunaciones
+    @GetMapping
+    public ResponseEntity <List<PetVaccination>> getAllPetVaccinations(){
+      return ResponseEntity.status(HttpStatus.OK).body(this.petVaccinationService.getAllPetVaccinations());
+    };
+
+    //Entpoint para activar obtener una vacunación por ID
+    @GetMapping(value = "/{id}")
+    public ResponseEntity <PetVaccination> getPetVaccinationById(@PathVariable Integer id){
+      return ResponseEntity.status(HttpStatus.OK).body(this.petVaccinationService.getPetVaccinationById(id));
+    };
+
+     //entpoint para activar eliminar una vacunación por id
+    @DeleteMapping("/{id}")
+    public ResponseEntity <Void> deleteVaccination(@PathVariable Integer id) {
+      petVaccinationService.deletePetVaccinationById(id);
+      return ResponseEntity.noContent().build();
     };
 }
--- src/main/java/dataPawBE/demo/controllers/VetController.java
+++ src/main/java/dataPawBE/demo/controllers/VetController.java
@@ -1,16 +1,18 @@
 package dataPawBE.demo.controllers;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import dataPawBE.demo.models.Vet;
 import dataPawBE.demo.services.VetService;
-
 
 
 @RestController
@@ -19,9 +21,36 @@ public class VetController {
     @Autowired
     VetService vetService;
 
+    //Entpoint para activar guardar un vet
     @PostMapping
     public ResponseEntity <Vet> save(@RequestBody Vet data){
       Vet saveResponseAPI = this.vetService.saveVet(data);
       return ResponseEntity.status(HttpStatus.OK).body(saveResponseAPI);
     };
+
+    //Entpoit para activar buscar por id el vet 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity <Vet> getVetById(@PathVariable Integer id){
+      return ResponseEntity.status(HttpStatus.OK).body(this.vetService.getVetById(id));
+    };
+
+    //Entpoit para activar buscar todos vet 
+    @GetMapping
+    public ResponseEntity <List<Vet>> getAllVets(){
+      return ResponseEntity.status(HttpStatus.OK).body(this.vetService.getAllVets());
+    };
+
+    //Entpoint para activar eliminar un vet por id
+    @DeleteMapping("/{id}")
+    public ResponseEntity <Void> deleteVet(@PathVariable Integer id) {
+      vetService.deleteVetById(id);
+      return ResponseEntity.noContent().build();
+    };
 }
+
+    
+
+
+
+
+  
